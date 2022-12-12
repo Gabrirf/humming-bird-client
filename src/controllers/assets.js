3273c5bf9fe5645400eaee9d300c7154609c4782
Gabriel <grodriguez@emergya.com>
2022-12-12 22:18:36 +0100

    feat(): Add route to activate all assets of a given accountId

--- src/controllers/assets.js
+++ src/controllers/assets.js
@@ -1,10 +1,34 @@
 const { hummingBirdService } = require('../services');
 
+const { arrayUtils } = require('../helpers');
+
 async function listAssets(req, res, next) {
   try {
     const { accountId } = req.query;
-    const createdUser = await hummingBirdService.getAssetsByAccount(accountId);
-    return res.status(200).send(createdUser);
+    const assetsList = await hummingBirdService.getAssetsByAccount(accountId);
+    return res.status(200).send(assetsList);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function activateAllAssets(req, res, next) {
+  try {
+    const { accountId } = req.body;
+    const assetsList = await hummingBirdService.getAssetsByAccount(accountId);
+
+    const reshapedAssets = arrayUtils.reshapeRows(assetsList, 10);
+    const assetsActivationResults = reshapedAssets.map(async (assetSet) => {
+      const results = [];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const asset of assetSet) {
+        // eslint-disable-next-line no-await-in-loop
+        const result = await hummingBirdService.activateAsset(asset.id, req.body);
+        results.push(result);
+      }
+    }).flat();
+
+    return res.status(200).send(assetsActivationResults);
   } catch (err) {
     return next(err);
   }
@@ -12,4 +36,5 @@ async function listAssets(req, res, next) {
 
 module.exports = {
   listAssets,
+  activateAllAssets,
 };
