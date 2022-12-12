1a63e6437f616e82a5b87d6f6838401e0bc265e4
Gabriel <grodriguez@emergya.com>
2022-12-12 17:17:53 +0100

    fix(): unwanted auth middleware

--- src/middlewares/security-handler.js
+++ src/middlewares/security-handler.js
@@ -1,8 +1,4 @@
-const bcrypt = require('bcryptjs');
 const { HttpStatusError } = require('common-errors');
-const { decodeBase64 } = require('../helpers');
-
-const { security } = require('../config');
 
 function ipAuth(req, trustedIps) {
   const requestIP = req.ip || req.connection.remoteAddress;
@@ -12,26 +8,6 @@ function ipAuth(req, trustedIps) {
   return Promise.resolve(true);
 }
 
-async function basicAuth(req) {
-  if (req.headers.authorization) {
-    const [, codedText] = req.headers.authorization.split(' ');
-    const decodedText = codedText ? decodeBase64(codedText) : '';
-    const [username, pass] = decodedText.split(':');
-    try {
-      /* Auth by env config */
-      const user = { username: security.user };
-      user.password = username === user.username ? await bcrypt.hash(security.pass, security.bcryptRounds) : '';
-      /** ******************* */
-      const validPassword = await bcrypt.compare(pass, user.password);
-      if (validPassword) return Promise.resolve(true);
-    } catch (err) {
-      throw Object.assign(err, { status: 401 });
-    }
-  }
-  throw new HttpStatusError(401, 'You must authenticate to access.');
-}
-
 module.exports = {
   ipAuth,
-  basicAuth,
 };
